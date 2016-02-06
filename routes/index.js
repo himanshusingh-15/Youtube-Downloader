f428cfe32ff48edb1452d90989ce10629575b5f4
Himanshu Singh <himanshusingh.singh06@gmail.com>
2016-02-07 00:57:53 +0530

    First commit

---
+++ routes/index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+fs = require('fs');
+youtubedl = require('youtube-dl');
+ffmpeg = require('fluent-ffmpeg');
+
+/* GET home page. */
+router.post('/', function(req, res, next) {
+	url = req.body.url;
+	youtubedl.getInfo(url, function(err, info) {
+		if(err)
+			throw err;
+		else {
+			console.log("himanshu");
+			audioFileName = "./public/download/" + info.title + ".mp3";
+			videoFileName = "./public/download/" + info.title + ".mp4";
+			youtubedl(url).pipe(fs.createWriteStream(videoFileName))
+						  .on('close', function() {
+						  	console.log("Finished Downloading");
+						  	res.send('Finished Downloading');
+						  });
+			/*proc = new ffmpeg({source:stream});
+			proc.setFfmpegPath('./public/ffmpeg');
+			proc.withAudioCodec('libmp3lame')
+			.toFormat('mp3')
+			.saveToFile(fileName, function(stdout, stderr) {
+				res.send('File Converted');
+			});
+			console.log("Completed");*/
+		}
+	});
+});
+
+module.exports = router;
